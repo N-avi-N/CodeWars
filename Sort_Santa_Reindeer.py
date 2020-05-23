4cf6255a6b5fcefb577a7b798a88cee0df62cf79
N-avi-N <navin_k212@yahoo.com>
2020-05-23 16:47:22 -0500

    Version Control Via Git. First Commit

---
+++ Sort_Santa_Reindeer.py
@@ -0,0 +1,3 @@
+def sort_reindeer(names):
+    return sorted(names, key = lambda x : x.split(' ')[-1].lower())
+
---
+++ test_Sort_Santa_Reindeer.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    def test_sort_reindeer(self):
+        from Sort_Santa_Reindeer import sort_reindeer
+        self.assertTrue(sort_reindeer(['aa bb']) == ['aa bb'])
+
+    def test_sort_reindeer_2(self):
+        from Sort_Santa_Reindeer import sort_reindeer
+        self.assertTrue(sort_reindeer(['aa bb', 'bb aa', 'cc dd', '']) == ['', 'bb aa', 'aa bb', 'cc dd'])
+
+    def test_sort_reindeer_3(self):
+        from Sort_Santa_Reindeer import sort_reindeer
+        self.assertTrue(sort_reindeer(["Dasher Tonoyan", "Dancer Moore", "Prancer Chua", "Vixen Hall", "Comet Karavani", "Cupid Foroutan", "Donder Jonker", "Blitzen Claus"]) ==
+                                        ["Prancer Chua", "Blitzen Claus", "Cupid Foroutan", "Vixen Hall", "Donder Jonker", "Comet Karavani", "Dancer Moore", "Dasher Tonoyan"])
